a75faa22d4b94a4af1265122926d2a0ecde19cf9
Michael DeSantis <m.desantis628@gmail.com>
2021-11-19 13:21:30 -0500

    studying testing and setup

--- code/testing-practice.js
+++ code/testing-practice.js
@@ -1,5 +1,3 @@
-const content = document.querySelector('.content');
-
 function capitalize(str) {
   let newStr = str.toUpperCase();
   return newStr[0] + str.slice(1).toLowerCase();
@@ -9,13 +7,13 @@ function reverse(str) {
   return capitalize(str.split('').reverse().join(''));
 }
 
-const calculator = (() => {
-  const add = (a, b) => a + b;
-  const subtract = (a, b) => a - b;
-  const multiply = (a, b) => a * b;
-  const divide = (a, b) => (b === 0 ? 'false' : a / b);
+function calculator(a, b) {
+  const add = a + b;
+  const subtract = a - b;
+  const multiply = a * b;
+  const divide = b === 0 ? 'false' : a / b;
   return { add, subtract, multiply, divide };
-})();
+}
 
 function cipher(str) {
   let hold = [];
@@ -34,3 +32,11 @@ function analyze(array) {
   const length = array.length;
   return { average, min, max, length };
 }
+
+module.exports = {
+  analyze,
+  cipher,
+  calculator,
+  reverse,
+  capitalize,
+};
